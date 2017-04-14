9f67b3f31507c3131a12ea5de0eb9822be47bb06
Marko Nikic <darkblue.azurite@gmail.com>
2017-04-14 18:07:04 +0200

    make react router compatible NavItems

--- client/js/components/Main/index.js
+++ client/js/components/Main/index.js
@@ -1,13 +1,42 @@
 const React = require("react");
+const { string, node } = require("prop-types");
+const { Navbar, Nav } = require("react-bootstrap");
+const { Link, NavLink } = require("react-router-dom");
 
 const styles = require("./styles.css");
+
+const NavItemLink = function({ to, children }) {
+  return(
+    <li role="presentation">
+      <NavLink to={to}>
+        {children}
+      </NavLink>
+    </li>
+  );
+};
+
+NavItemLink.propTypes = {
+  to: string,
+  children: node
+};
 
 const Main = React.createClass({
   render: function() {
     return(
-      <h3 className={styles.helloWorld}>
-        Hello World
-      </h3>
+      <Navbar className={styles.sharpEdged}>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Pinterest Clone</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItemLink to="/login">Login</NavItemLink>
+            <NavItemLink to="/register">Register</NavItemLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 });
