f37de2aa5f5f45b9f71b02b2986b470e3f2c6e33
Marko Nikic <darkblue.azurite@gmail.com>
2017-04-17 12:39:18 +0200

    change routes rendering

--- client/js/routes.js
+++ client/js/routes.js
@@ -2,7 +2,7 @@ const React = require("react");
 const { Route } = require("react-router");
 const Main = require("./components/Main");
 
-let Pinwall, Login, Register;
+let Pinwall, Login, Register, Profile;
 
 if(process.env.LAZY_LOAD) {
   const lazyload = require("./components/Utils/HOC/lazyload");
@@ -11,23 +11,28 @@ if(process.env.LAZY_LOAD) {
   Pinwall = lazyload(loaders.Pinwall);
   Login = lazyload(loaders.Login);
   Register = lazyload(loaders.Register);
+  Profile = lazyload(loaders.Profile);
 
 }
 else {
   Pinwall = require("./components/Pinwall");
   Login = require("./components/Login");
   Register = require("./components/Register");
+  Profile = require("./components/Profile");
 }
 
-const routes = function() {
-  return(
-    <div>
-      <Route path="/" component={Main}/>
-      <Route exact path="/" component={Pinwall}/>
-      <Route path="/login" component={Login}/>
-      <Route path="/register" component={Register}/>
-    </div>
-  );
-};
+const Routes = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <Route path="/" component={Main}/>
+        <Route exact path="/" component={Pinwall}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/user" component={Profile}/>
+      </div>
+    );
+  }
+});
 
-module.exports = routes;
+module.exports = Routes;
