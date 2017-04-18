368769113e7f89f6a7cfb490099d4ce69c370fdf
Marko Nikic <darkblue.azurite@gmail.com>
2017-04-18 14:20:19 +0200

    add user action

--- client/js/actions/types.js
+++ client/js/actions/types.js
@@ -1,3 +1,3 @@
 module.exports = {
-  
+  UPDATE_USER: "UPDATE_USER"
 };
--- client/js/reducers/user.js
+++ client/js/reducers/user.js
@@ -1,5 +1,9 @@
+const types = require("../actions/types");
+
 const user = function(state, action) {
   switch(action.type) {
+    case types.UPDATE_USER:
+      return action.user;
     default:
       return state;
   }
