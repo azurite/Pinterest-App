35374161afa98546016e5922bdefe37f9dc57539
Marko Nikic <darkblue.azurite@gmail.com>
2017-04-23 16:02:15 +0200

    add broken image replacement

--- client/js/components/Pin/index.js
+++ client/js/components/Pin/index.js
@@ -6,50 +6,60 @@ const { Image } = require("react-bootstrap");
 const styles = require("./styles.css");
 const { debounce } = require("../../lib/tools");
 
-const Pin = function(props) {
-  let { id, image_url, description, liked_by } = props.data;
-  let { remove, spinner, toggleLike, liked } = props;
-  return(
-    <div className={styles.pinContainer}>
-      <Image className={styles.pinImage} src={image_url} responsive/>
-      <hr/>
-      <div className={styles.descContainer}>
-        <h3>{description}</h3>
-        <span className="pull-right">
-          <i
-            className={"fa fa-heart " + styles.pointer + (liked ? " " + styles.liked : "")}
-            onClick={toggleLike ? debounce(
-              toggleLike.bind(null, liked ? "unlike" : "like", id),
-              1000,
-              true
-            ) : f => f}
-          />
-          {" " + liked_by.length}
-        </span>
-        {
-          remove &&
-          <i
-            className={"fa fa-trash-o " + styles.pointer}
-            onClick={remove.bind(null, id)}
-          />
-        }
-        {" "}
-        {
-          spinner &&
-          <i className="fa fa-spinner fa-spin"/>
-        }
-        <div className={styles.clear}/>
+const Pin = React.createClass({
+  propTypes: {
+    toggleLike: func,
+    liked: bool,
+    data: pin,
+    remove: func,
+    spinner: bool
+  },
+  replaceBrokenImage: function(e) {
+    e.target.src = "/media/pin_placeholder.png";
+    this.forceUpdate();
+  },
+  render: function() {
+    let { id, image_url, description, liked_by } = this.props.data;
+    let { remove, spinner, toggleLike, liked } = this.props;
+    return(
+      <div className={styles.pinContainer}>
+        <Image
+          onError={this.replaceBrokenImage}
+          className={styles.pinImage}
+          src={image_url}
+          responsive
+        />
+        <hr/>
+        <div className={styles.descContainer}>
+          <h3>{description}</h3>
+          <span className="pull-right">
+            <i
+              className={"fa fa-heart " + styles.pointer + (liked ? " " + styles.liked : "")}
+              onClick={toggleLike ? debounce(
+                toggleLike.bind(null, liked ? "unlike" : "like", id),
+                1000,
+                true
+              ) : f => f}
+            />
+            {" " + liked_by.length}
+          </span>
+          {
+            remove &&
+            <i
+              className={"fa fa-trash-o " + styles.pointer}
+              onClick={remove.bind(null, id)}
+            />
+          }
+          {" "}
+          {
+            spinner &&
+            <i className="fa fa-spinner fa-spin"/>
+          }
+          <div className={styles.clear}/>
+        </div>
       </div>
-    </div>
-  );
-};
-
-Pin.propTypes = {
-  toggleLike: func,
-  liked: bool,
-  data: pin,
-  remove: func,
-  spinner: bool
-};
+    );
+  }
+});
 
 module.exports = Pin;
