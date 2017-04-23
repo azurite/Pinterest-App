31299368c9748b79dbfad2ed7369b07fce2b2727
Marko Nikic <darkblue.azurite@gmail.com>
2017-04-23 17:01:49 +0200

    move pareQuery to lib/tools and require it instead

--- client/js/lib/tools/index.js
+++ client/js/lib/tools/index.js
@@ -1,9 +1,11 @@
 const pageBottom = require("./page-bottom");
 const debounce = require("./debounce");
 const callOnce = require("./call-once");
+const parseQuery = require("./parse-query");
 
 module.exports = {
   pageBottom,
   debounce,
-  callOnce
+  callOnce,
+  parseQuery
 };
